9f6172a5c4c59b5800ad2c20a60a27aa8ee56c91
durai <nsmani.qa@gmail.com>
2017-06-23 16:11:44 -0400

    new updated code

--- AddToCartPage.java
+++ AddToCartPage.java
@@ -11,29 +11,7 @@ import common.TestCaseConfiguration;
 
 public class AddToCartPage extends TestCaseConfiguration {
 
-	@FindBy(xpath="//*[@id='Table2']/tbody/tr[2]/td[1]")
-	WebElement firstrowitem_title;
-	@FindBy(xpath="//*[@id='Table2']/tbody/tr[6]/td[2]")
-	WebElement ultimatedvdpack_addtocatrprice;
-
-	public AddToCartPage ()
-	{
-		PageFactory.initElements(driver, this);
-	}
-	public void priceverification(){
-		if(Utility.isEqual("$29.85USD",ultimatedvdpack_addtocatrprice.getText()))
-		{
-			Reports.log(LogStatus.PASS, "add to cart price is matching");
-		}
-		else
-			Reports.log(LogStatus.FAIL, "add to cart price is not matching");
-		
-		if(Utility.isEqual("21 Day Fix Ultimate DVD Package",firstrowitem_title.getText()))
-		{
-			Reports.log(LogStatus.PASS, "add to cart title is matching");
-		}
-		else
-			Reports.log(LogStatus.FAIL, "add to cart title is not matching");
+	@F
 		
 	}
 	
