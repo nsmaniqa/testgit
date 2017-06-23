3e864eab7c5cae01d7e8eebcbe38e215a6b059ba
durai <nsmani.qa@gmail.com>
2017-06-23 16:21:53 -0400

    updte all final

--- AddToCartPage.java
+++ AddToCartPage.java
@@ -1,18 +1,2 @@
-package page;
-
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.PageFactory;
-
-import com.relevantcodes.extentreports.LogStatus;
-
-import tool.Utility;
-import common.TestCaseConfiguration;
-
-public class AddToCartPage extends TestCaseConfiguration {
-
-	@F
-		
-	}
-	
-}
+dsddsd
+sdsdfds
